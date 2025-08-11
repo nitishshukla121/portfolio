ea07abfcf79bbef4a8a65a4992f8785d745b1c51
Nitish Shukla <nitish121shukla@gmail.com>
2025-08-12 01:17:57 +0530

    Initial commit

--- src/main/java/com/example/Spring/Thymeleaf/ThymeleafPracticeApplication.java
+++ src/main/java/com/example/Spring/Thymeleaf/ThymeleafPracticeApplication.java
@@ -3,9 +3,7 @@ package com.example.Spring.Thymeleaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
-@EnableJpaRepositories("com.example.Spring.Repository")
 @EntityScan("com.example.Spring.Entity")
 public class ThymeleafPracticeApplication {
 
